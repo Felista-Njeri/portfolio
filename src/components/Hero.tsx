f6117264341682b5fa6583f4e7b837a27e137759
Felista-Njeri <njeerikamau2@gmail.com>
2025-11-05 02:38:10 +0300

    add id to hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import { scrollToSection } from "@/lib/scrollToSection";
 
 const Hero = () => {
   return (
-    <section className="min-h-screen flex items-center justify-center pt-16 px-4 sm:px-6 lg:px-8">
+    <section id="hero" className="min-h-screen flex items-center justify-center pt-16 px-4 sm:px-6 lg:px-8">
       <div className="container mx-auto">
         <div className="relative">
           {/* Decorative element */}
